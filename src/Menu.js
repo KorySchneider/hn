85ea7c824ae94e4b96bfbf0774c3353d3f4dec95
Kory Schneider <kory.schneider@gmail.com>
2019-09-26 15:35:13 -0700

    Adjust menu style

--- src/Menu.js
+++ src/Menu.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 
+const style = {
+  margin: '2em auto',
+  padding: '0.5em',
+};
+
 function Menu() {
   return (
-    <Paper style={{marginTop: '2em'}} elevation={2} align='center'>
+    <Paper style={style} elevation={2} align='center'>
       <Button onClick={() => console.log('press')}>Top</Button>
       <Button onClick={() => console.log('press')}>New</Button>
       <Button onClick={() => console.log('press')}>Best</Button>
