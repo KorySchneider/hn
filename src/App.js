412b0b7d9cbe56fd12cdfdeb35606f4f94536584
Kory Schneider <kory.schneider@gmail.com>
2019-09-26 19:21:36 -0700

    Show text posts with preview

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
-import Menu from './Menu';
 import Story from './Story';
 
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 
 const url = 'https://hacker-news.firebaseio.com/v0';
+const pageSize = 20;
+
+const menuStyle = {
+  margin: '2em auto',
+  padding: '0.5em',
+};
 
 function App() {
+  const [section, setSection] = useState('ask');
   const [topStories, setTopStories] = useState([]);
-  const [visibleItems, setVisibleItems] = useState([]);
-  const [pageSize, setPageSize] = useState(25);
-  const [pageIndex, setPageIndex] = useState(pageSize);
+  const [newStories, setNewStories] = useState([]);
+  const [bestStories, setBestStories] = useState([]);
+  const [askStories, setAskStories] = useState([]);
+  const [showStories, setShowStories] = useState([]);
+  const [jobStories, setJobStories] = useState([]);
+  const [visibleStories, setVisibleStories] = useState([]);
+
+  const [commentsOpen, setCommentsOpen] = useState(false);
+  const [commentsParent, setCommentsParent] = useState(null);
 
   async function fetchItem(id) {
     const response = await fetch(`${url}/item/${id}.json`);
@@ -20,8 +34,8 @@ function App() {
   }
 
   useEffect(() => {
-    async function fetchTopStories() {
-      const response = await fetch(url + '/topstories.json');
+    async function fetchStories() {
+      const response = await fetch(url + `/${section}stories.json`);
       const results = await response.json();
 
       let items = [];
@@ -30,17 +44,32 @@ function App() {
         items.push(item);
       }
 
-      setVisibleItems(items);
-      setTopStories(results);
+      setVisibleStories(items);
       return results;
     }
-    fetchTopStories();
+    fetchStories();
   }, [])
+
+  function handleMenuClick(event) {
+    setSection(event.currentTarget.id);
+  }
 
   return (
     <Container maxWidth='md'>
-      <Menu />
-      {visibleItems.map(item => <Story data={item} key={item.title} />)}
+      {/* Menu */}
+      <Paper style={menuStyle} elevation={2} align='center'>
+        <Button id='top' onClick={e => handleMenuClick(e)}>Top</Button>
+        <Button id='new' onClick={e => handleMenuClick(e)}>New</Button>
+        <Button id='best' onClick={e => handleMenuClick(e)}>Best</Button>
+        <Button id='ask' onClick={e => handleMenuClick(e)}>Ask</Button>
+        <Button id='show' onClick={e => handleMenuClick(e)}>Show</Button>
+        <Button id='job' onClick={e => handleMenuClick(e)}>Jobs</Button>
+      </Paper>
+
+      {/* Stories */}
+      {visibleStories.map(item => <Story data={item} key={item.title} />)}
+
+      {/* Comments */}
     </Container>
   );
 }
--- src/Story.js
+++ src/Story.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
 import Slide from '@material-ui/core/Slide';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 function Story(props) {
@@ -30,8 +30,19 @@ function Story(props) {
         }}
       >
         <CardContent>
-          <Typography variant='h6'>
+          <Typography variant='h5'>
             {props.data.title}
+          </Typography>
+          <Typography>
+            {expanded && props.data.text}
+            {/*expanded &&
+              <Button onClick={showComments(props.data.id)}>
+                Comments
+              </Button>
+              */}
+            {!expanded && props.data.text &&
+              props.data.text.substring(0, 80) + '...'
+            }
           </Typography>
         </CardContent>
       </Card>
