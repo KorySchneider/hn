febc06bed13ded66bf9133d73f956550d659bd2b
Kory Schneider <kory.schneider@gmail.com>
2019-09-27 17:27:03 -0700

    Updates

---
+++ precache-manifest.1fe7fbd30b2c7d3ed81dd6c3df2e4d6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa208a187a1ed08da27c5e845f32e5b",
+    "url": "/hn/index.html"
+  },
+  {
+    "revision": "ffebf3547c0c8f1aeb3c",
+    "url": "/hn/static/css/main.cfbcba23.chunk.css"
+  },
+  {
+    "revision": "95775568838c4370e695",
+    "url": "/hn/static/js/2.94034c67.chunk.js"
+  },
+  {
+    "revision": "ffebf3547c0c8f1aeb3c",
+    "url": "/hn/static/js/main.f634d4d1.chunk.js"
+  },
+  {
+    "revision": "c1d9847e0b6131fad5e5",
+    "url": "/hn/static/js/runtime-main.7008b6d7.js"
+  }
+]);
