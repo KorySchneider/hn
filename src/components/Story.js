3e1cdfd11a3e243f58d98473379947fe0e7d204d
Kory Schneider <kory.schneider@gmail.com>
2019-10-01 17:34:56 -0700

    Style story cards

--- src/components/Story.js
+++ src/components/Story.js
@@ -4,22 +4,22 @@ import moment from 'moment';
 import h2p from 'html2plaintext';
 
 import Slide from '@material-ui/core/Slide';
+import Container from '@material-ui/core/Container';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const subtextStyle = {
-  marginRight: '20px',
+  marginLeft: '20px',
+  padding: '5px',
 };
 
 function Story({ data, timeout }) {
   const [expanded, setExpanded] = useState(false);
-  const [contentViewed, setContentViewed] = useState(false);
 
   const handleClick = (e) => {
     e.stopPropagation();
-    setContentViewed(true);
     if (data.url) window.open(data.url);
     if (data.text) setExpanded(expanded => !expanded);
   }
@@ -49,29 +49,31 @@ function Story({ data, timeout }) {
           <Typography variant='h6'>
             {h2p(data.title)}
           </Typography>
+          <Typography variant='caption' align='right' gutterBottom>
+            {data.url && data.url.split('/')[2].replace(/^www\./, '')}
+          </Typography>
           <Typography>
             {expanded && h2p(data.text)}
             {!expanded && data.text &&
               h2p(data.text.substring(0, 70) + '...')
             }
           </Typography>
-          <Typography variant='caption' display='block' gutterBottom>
-            {data.url && data.url.split('/')[2].replace(/^www\./, '')}
-          </Typography>
-          <Typography variant='overline' style={subtextStyle}>
-            {data.score + ' points'}
-          </Typography>
-          <Typography variant='overline' style={subtextStyle}>
-            {moment.unix(data.time).fromNow()}
-          </Typography>
-          <Button
-            onClick={e => openComments(e)}
-            style={subtextStyle}
-            size='small'
-            disabled={!contentViewed}
-          >
-            {(data.kids && data.kids.length + ' replies') || '0 replies'}
-          </Button>
+
+          <Container align='right'>
+            <Typography variant='overline' style={subtextStyle}>
+              {data.score + ' points'}
+            </Typography>
+            <Typography variant='overline' style={subtextStyle}>
+              {moment.unix(data.time).fromNow()}
+            </Typography>
+            <Button
+              onClick={e => openComments(e)}
+              style={{ fontWeight: 400, ...subtextStyle }}
+              size='small'
+            >
+              {(data.kids && data.kids.length + ' replies') || '0 replies'}
+            </Button>
+          </Container>
         </CardContent>
       </Card>
     </Slide>
