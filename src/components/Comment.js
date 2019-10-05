c8297cb23f831830432b9e9065ad6269fd4e566f
Kory Schneider <kory.schneider@gmail.com>
2019-10-04 17:22:15 -0700

    Use CardActions in Comment components

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -5,6 +5,7 @@ import h2p from 'html2plaintext';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
@@ -28,6 +29,9 @@ function Comment({ data }) {
             {h2p(p)}
           </Typography>
         ))}
+      </CardContent>
+
+      <CardActions>
         <Container align='right'>
           <Typography variant='overline' style={subtextStyle}>
             &mdash; {data.by}
@@ -44,7 +48,7 @@ function Comment({ data }) {
             {(data.kids && data.kids.length + ' replies') || '0 replies'}
           </Button>
         </Container>
-      </CardContent>
+      </CardActions>
     </Card>
   )
 }
