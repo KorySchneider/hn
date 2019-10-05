09a700837e7874cb30e550c6219cc5c210a17543
Kory Schneider <kory.schneider@gmail.com>
2019-10-05 14:24:15 -0700

    Work on comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import moment from 'moment';
 import h2p from 'html2plaintext';
@@ -9,6 +9,7 @@ import CardActions from '@material-ui/core/CardActions';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Slide from '@material-ui/core/Slide';
 
 const style = {
   margin: '1em',
@@ -20,36 +21,62 @@ const subtextStyle = {
   padding: '5px',
 };
 
-function Comment({ data }) {
-  return (
-    <Card style={style} raised>
-      <CardContent>
-        {data.text && data.text.split('<p>').map((p, i) => (
-          <Typography gutterBottom key={p+i}>
-            {h2p(p)}
-          </Typography>
-        ))}
-      </CardContent>
+function Comment({ data, depth }) {
 
-      <CardActions>
-        <Container align='right'>
-          <Typography variant='overline' style={subtextStyle}>
-            &mdash; {data.by}
-          </Typography>
-          <Typography variant='overline' style={subtextStyle}>
-            {moment.unix(data.time).fromNow()}
-          </Typography>
-          <Button
-            size='small'
-            style={{ fontWeight: 400, ...subtextStyle }}
-            onClick={() => console.log('TODO load child comments')}
-            disabled={!data.kids || (data.kids && data.kids.length === 0)}
-          >
-            {(data.kids && data.kids.length + ' replies') || '0 replies'}
-          </Button>
-        </Container>
-      </CardActions>
-    </Card>
+  const [childComments, setChildComments] = useState([]);
+
+  const loadChildComments = () => {
+    console.log('test');
+  };
+
+  return (
+    <span>
+      <Slide
+        direction='up'
+        in={data.id >= 0}
+        mountOnEnter
+        unmountOnExit
+      >
+        <Card style={style} raised>
+          <CardContent>
+            {data.id}
+            {data.text && data.text.split('<p>').map((p, i) => (
+              <Typography gutterBottom key={p+i}>
+                {h2p(p)}
+              </Typography>
+            ))}
+          </CardContent>
+
+          <CardActions>
+            <Container align='right'>
+              <Typography variant='overline' style={subtextStyle}>
+                &mdash; {data.by}
+              </Typography>
+              <Typography variant='overline' style={subtextStyle}>
+                {moment.unix(data.time).fromNow()}
+              </Typography>
+              <Button
+                size='small'
+                style={{ fontWeight: 400, ...subtextStyle }}
+                onClick={loadChildComments}
+                disabled={!data.kids || (data.kids && data.kids.length === 0)}
+              >
+                {(data.kids && data.kids.length + ' replies') || '0 replies'}
+              </Button>
+            </Container>
+          </CardActions>
+        </Card>
+      </Slide>
+
+      {childComments.map((item, i) => (
+        <Comment
+          data={item}
+          key={item.by + item.id}
+          timeout={i * 50}
+          depth={depth + 1}
+        />
+      ))}
+    </span>
   )
 }
 
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux';
 import { updateCommentsOpen } from '../redux/actions';
 import { url } from '../redux/store';
+
+import { useBottomScrollListener } from 'react-bottom-scroll-listener';
 import h2p from 'html2plaintext';
 
 import Comment from './Comment';
@@ -15,6 +17,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
+import Slide from '@material-ui/core/Slide';
 
 const mapState = state => ({
   commentsOpen: state.commentsOpen,
@@ -33,6 +36,14 @@ function Comments({
   updateCommentsOpen,
 }) {
   const [visibleComments, setVisibleComments] = useState([]);
+  const [commentsPage, setCommentsPage] = useState(1);
+  const commentsPageSize = 10;
+
+  const scrollRef = useBottomScrollListener(() => {
+    if (visibleComments.length <= commentsPage * commentsPageSize) {
+      setCommentsPage(commentsPage => ++commentsPage);
+    }
+  });
 
   async function fetchItem(id) {
     const response = await fetch(`${url}/item/${id}.json`);
@@ -42,24 +53,47 @@ function Comments({
 
   useEffect(() => {
     async function fetchComments() {
-      let items = [];
-      let numToFetch = comments.length < 10 ? comments.length : 10;
-      for (let i = 0; i < numToFetch; i++) {
-        const item = await fetchItem(comments[i]);
-        items.push(item);
+      console.log(commentsPage, commentsPageSize, visibleComments.length);
+      if (visibleComments.length === 0 ||
+      (visibleComments.length <= (commentsPage - 1) * commentsPageSize &&
+      comments.length >= commentsPage * commentsPageSize)) {
+        console.log('fetch comments');
+        let items = [];
+        const ids = comments.slice(
+          (commentsPage * commentsPageSize) - commentsPageSize,
+          commentsPage * commentsPageSize
+        );
+        for (let i = 0; i < ids.length; i++) {
+          const item = await fetchItem(ids[i]);
+          items.push(item);
+        }
+        // Filter duplicate and null items
+        items = items.filter(item => {
+          if (item === null) return false;
+          for (let i = 0; i < visibleComments.length; i++) {
+            if (item.id === visibleComments[i].id)
+              return false;
+          }
+          return true;
+        });
+        if (items.length > 0) {
+          setVisibleComments(visibleComments.concat(items));
+        }
       }
-      items = items.filter(item => !item.dead && !item.deleted);
-      setVisibleComments(items);
     }
     fetchComments();
-  }, [comments]);
+  }, [comments, commentsPage, visibleComments]);
 
   if (commentsParent === null) return null;
   return (
     <Modal
       open={commentsOpen}
-      onClose={() => updateCommentsOpen(false)}
+      onClose={() => {
+        updateCommentsOpen(false);
+        setVisibleComments([]);
+      }}
       style={{ overflowY: 'scroll' }}
+      ref={scrollRef}
     >
       <Container
         maxWidth='md'
@@ -69,33 +103,40 @@ function Comments({
           padding: '5px',
         }}
       >
-        <Card raised>
-          <CardContent style={{ padding: '1em' }}>
-            <Typography variant='h5'>
-              {commentsParent.title}
-            </Typography>
-            {commentsParent.url &&
-              <Typography variant='caption'>
-                {commentsParent.url.split('/')[2].replace(/^www\./, '')}
+        <Slide
+          direction='up'
+          mountOnEnter
+          unmountOnExit
+          in={commentsOpen}
+        >
+          <Card raised>
+            <CardContent style={{ padding: '1em' }}>
+              <Typography variant='h5'>
+                {commentsParent.title}
               </Typography>
-            }
-            {commentsParent.text &&
-              commentsParent.text.split('<p>').map(p => (
-                <Typography key={p}>
-                  {h2p(p)}
+              {commentsParent.url &&
+                <Typography variant='caption'>
+                  {commentsParent.url.split('/')[2].replace(/^www\./, '')}
                 </Typography>
-              ))
-            }
-          </CardContent>
+              }
+              {commentsParent.text &&
+                commentsParent.text.split('<p>').map(p => (
+                  <Typography key={p}>
+                    {h2p(p)}
+                  </Typography>
+                ))
+              }
+            </CardContent>
 
-          <CardActions>
-            <Container align='right'>
-              <Typography variant='overline'>
-                &mdash; {commentsParent.by}
-              </Typography>
-            </Container>
-          </CardActions>
-        </Card>
+            <CardActions>
+              <Container align='right'>
+                <Typography variant='overline'>
+                  &mdash; {commentsParent.by}
+                </Typography>
+              </Container>
+            </CardActions>
+          </Card>
+        </Slide>
 
         <Fab
           onClick={() => updateCommentsOpen(false)}
@@ -110,14 +151,18 @@ function Comments({
           <CloseIcon />
         </Fab>
 
-        {visibleComments.length === 0 &&
-          <Container align='center'>
-            <CircularProgress style={{margin: '50% auto'}} />
-          </Container>
-        }
-        {visibleComments.map(item => (
-          <Comment data={item} key={item.id} />
+        {visibleComments.map((item, i) => (
+          <Comment
+            data={item}
+            key={item.by + item.id}
+            timeout={0}
+            depth={0}
+          />
         ))}
+
+        <Container align='center'>
+          <CircularProgress style={{ margin: '2em' }} />
+        </Container>
       </Container>
     </Modal>
   )
