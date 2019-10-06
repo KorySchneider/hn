bcb7a938c0a58954925d124f7804046740b6d5cc
Kory Schneider <kory.schneider@gmail.com>
2019-10-05 17:27:03 -0700

    Fix page load

--- src/components/App.js
+++ src/components/App.js
@@ -44,7 +44,7 @@ function App({
 
   useBottomScrollListener(() => {
     if (visibleStories.length === page * pageSize) {
-      updateCurrentPage(page => page + 1);
+      updateCurrentPage(page + 1);
     }
   }, 500, 200)
 
